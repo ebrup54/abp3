efcc2da8f6396033e8d6f70df11eb6e1a7bee8af
ebrupeker1 <ebrupeker1@outlook.com>
2022-07-28 15:53:34 +0300

    asd

--- angular/src/app/proje/proje.component.ts
+++ angular/src/app/proje/proje.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, InjectDecorator, Injector, OnInit } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { ProjeDto, ProjeServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
   selector: 'app-proje',
   templateUrl: './proje.component.html',
   styleUrls: ['./proje.component.css']
 })
-export class ProjeComponent implements OnInit {
+export class ProjeComponent extends AppComponentBase implements OnInit {
 
-  constructor() { }
+  projeler: ProjeDto []= [];
+
+  constructor(
+    injector: Injector,
+    private _projeServiceProxy: ProjeServiceProxy
+    ) {
+    super(injector)
+   }
 
   ngOnInit(): void {
+    this._projeServiceProxy.getProjeList()
+    .subscribe((res) => {
+      this.projeler = res;
+    })
   }
 
 }
